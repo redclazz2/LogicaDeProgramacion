c20d81554d842fbe215936c59ca9a5e5564e75a5
Sebastian Zarate <63659954+redclazz2@users.noreply.github.com>
2021-03-21 19:45:26 -0500

    Delete Taller19MarzoPunto9.py

--- Taller19MarzoPunto9.py
+++
@@ -1,9 +0,0 @@
-print("Punto 9")
-numero1 = int(input("Ingrese un número entero positivo: "))
-
-f = 1
-if numero1 != 0:
-    for x in range(1, numero1 + 1):
-        f = f * x
-
-print("El número factorial es: ", f)
