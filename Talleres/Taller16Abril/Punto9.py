5b1256904b7962c7b228ab7408c8abe522a6809e
Sebastian Zarate <63659954+redclazz2@users.noreply.github.com>
2021-04-19 22:34:58 -0500

    Update Punto9

--- Talleres/Taller16Abril/Punto9.py
+++ Talleres/Taller16Abril/Punto9.py
@@ -1,6 +1,6 @@
 def verificadorCedula(cedula :str):
-    if len(cedula) == 8 or len(cedula)==10:
-        print("La cedula es válida pues tiene 8 o 10 dígitos.")
+    if len(cedula) == 8 or len(cedula)==10 or len(cedula)==7:
+        print("La cedula es válida pues tiene 7, 8 o 10 dígitos.")
     else:
         print("La cedula no es válida!")
 inputCedula = input("Ingrese el número de la cedula para verificar que sea válida: ")
